a8538e7b58f21e3e87846676e7f1fc0a705bde9d
oguru <oguru@hotmail.co.uk>
2020-04-06 22:46:55 +0100

    mobile about done

--- public/main.js
+++ public/main.js
@@ -1,5 +1,11 @@
 // const getArrows = document.getElementsByClassName("arrow");
 
+const changeOOP = () => {
+   document.getElementById("oop").innerHTML = "Object oriented programming"
+}
+
+screen.width > 374 ? changeOOP() : null;
+
 
 // class Arrow {
 // constructor(id) {
@@ -46,48 +52,74 @@ const arrows = document.getElementsByClassName("arrowspan");
 
 showArrow = () => {
    arrows[0].classList.add("borderClass");
-   arrows[1].classList.add("borderClass");
-   arrows[2].classList.add("borderClass");
-   arrows[3].classList.add("borderClass");
-   arrows[4].classList.add("borderClass");
-   arrows[5].classList.add("borderClass");
+   // arrows[1].classList.add("borderClass");
+   // arrows[2].classList.add("borderClass");
+   // arrows[3].classList.add("borderClass");
+   // arrows[4].classList.add("borderClass");
+   // arrows[5].classList.add("borderClass");
 }
 
 removeArrow = () => {
    arrows[0].classList.remove("borderClass");
-   arrows[1].classList.remove("borderClass");
-   arrows[2].classList.remove("borderClass");
-   arrows[3].classList.remove("borderClass");
-   arrows[4].classList.remove("borderClass");
-   arrows[5].classList.remove("borderClass");
+   // arrows[1].classList.remove("borderClass");
+   // arrows[2].classList.remove("borderClass");
+   // arrows[3].classList.remove("borderClass");
+   // arrows[4].classList.remove("borderClass");
+   // arrows[5].classList.remove("borderClass");
 }
 
-const navIcon = document.getElementById("nav-icon");
-const navLines = document.getElementsByClassName("nav-line");
-const navText = document.getElementsByClassName("nav-text");
+const navIcon = document.querySelector("#nav-icon");
+const navLines = document.querySelectorAll(".nav-line");
+const navText = document.querySelectorAll(".nav-text");
 
 const moveNav = () => {
+
    navIcon.classList.add("moveNavIcon");
-   navLines[0].classList.add("navLineAnimate")
-   navLines[1].classList.add("navLineAnimate")
-   navLines[2].classList.add("navLineAnimate")
-   navText[0].classList.add("bottom-zero")
-   navText[1].classList.add("bottom-zero")
-   navText[2].classList.add("bottom-zero")
-   navText[3].classList.add("bottom-zero")
+
+   for (let i = 0; i < 3; i++) {
+      navLines[i].classList.add("navLineAnimate")
+   }
+
+   for (let i = 0; i < navText.length; i++) {
+      navText[i].classList.add("bottom-zero");
+   }
 }
 
 const returnNav = () => {
+
    navIcon.classList.remove("moveNavIcon");
-   navLines[0].classList.remove("navLineAnimate")
-   navLines[1].classList.remove("navLineAnimate")
-   navLines[2].classList.remove("navLineAnimate")
-   navText[0].classList.remove("bottom-zero")
-   navText[1].classList.remove("bottom-zero")
-   navText[2].classList.remove("bottom-zero")
-   navText[3].classList.remove("bottom-zero")
+
+   for (let i = 0; i < 3; i++) {
+      navLines[i].classList.remove("navLineAnimate")
+   }
+
+   for (let i = 0; i < navText.length; i++) {
+      navText[i].classList.remove("bottom-zero");
+   }
 }
 
+const goToTopIcon = document.querySelector("#go-to-top-icon");
+const botNavText = document.querySelector("#top-txt");
+
+const moveBotNav = () => {
+   goToTopIcon.classList.add("moveGoToTopIcon");
+
+   for (let i = 3; i < 6; i++) {
+      navLines[i].classList.add("navLineAnimate")
+   }
+
+   botNavText.classList.add("move-bot-nav-txt");
+ }
+
+const returnBotNav = () => {
+   goToTopIcon.classList.remove("moveGoToTopIcon");
+
+   for (let i = 3; i < 6; i++) {
+      navLines[i].classList.remove("navLineAnimate")
+   }
+
+   botNavText.classList.remove("move-bot-nav-txt");
+}
 
 
 
