2285ed7fdb8d73e3b631e6b050d545a6377cfb4a
oguru <oguru@hotmail.co.uk>
2020-12-01 14:13:54 +0000

    updated recaptcha

--- public/main.js
+++ public/main.js
@@ -2,17 +2,12 @@
 let viewWidth = document.getElementsByTagName("body")[0].clientWidth;
 
 window.onload = function () {
-   document.getElementsByClassName('contact-form')[0].addEventListener('submit', function (event) {
-      event.preventDefault();
-      // generate the contact number value
-      this.contact_number.value = Math.random() * 100000 | 0;
-      emailjs.sendForm('gmail', 'portfolio', this);
-   });
+   contactFormHandler();   
 }
 
-function recaptchaCallback(token) {
-   token.length > 0 ? console.log(token.length) : ""
-}
+// function recaptchaCallback(token) {
+//    token.length > 0 ? console.log(token.length) : ""
+// }
 
 const ignorePointer = (ele, index) => {
    if (index) {
@@ -84,7 +79,7 @@ const returnNav = (event) => {
       navIcon.classList.remove("moveNavIcon");
 
       for (let i = 0; i < 3; i++) {
-         navLines[i].classList.remove("navLineAnimate")
+         navLines[i].classList.remove("navLineAnimate");
       }
 
    }
@@ -128,7 +123,7 @@ const returnNavMob = (event) => {
    navIcon.classList.remove("moveNavIcon");
 
    for (let i = 0; i < 3; i++) {
-      navLines[i].classList.remove("navLineAnimate")
+      navLines[i].classList.remove("navLineAnimate");
    }
 
    navBar.classList.add("nav-delay");
@@ -143,16 +138,16 @@ const returnNavMob = (event) => {
 const urlAddress = window.location.href;
 
 const clickNavLink = (event, elementId) => {
-   event.stopPropagation()
+   event.stopPropagation();
    ignorePointer(navBar);
-   navText.forEach(link => ignorePointer(link))
+   navText.forEach(link => ignorePointer(link));
    returnNavMob();
    setTimeout(() => {
       urlAddress.includes("peterdev") ? (
       window.location.assign(`https://peterdev.co.uk/#${elementId}`)
       ) : (
       window.location.assign(`http://127.0.0.1:5501/public/index.html#${elementId}`));
-         navText.forEach(link => resumePointer(link))
+         navText.forEach(link => resumePointer(link));
          resumePointer(navBar);
    }, 1000);
 }
@@ -172,14 +167,14 @@ const returnToTop = (event) => {
       setTimeout(() => window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }), 1000);
    }
    else {
-      window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+      window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
    }
 }
 
 const moveNav = (input) => {
 
    if (input === "mouse" && viewWidth < 768) {
-      return
+      return;
    }
    else {
    // if (viewWidth > 768) {
@@ -229,7 +224,7 @@ const moveBotNav = () => {
    goToTopIcon.classList.add("moveGoToTopIcon");
 
    for (let i = 3; i < 6; i++) {
-      navLines[i].classList.add("navLineAnimate")
+      navLines[i].classList.add("navLineAnimate");
    }
 
    botNavText.classList.add("move-bot-nav-txt");
@@ -239,7 +234,7 @@ const returnBotNav = () => {
    goToTopIcon.classList.remove("moveGoToTopIcon");
 
    for (let i = 3; i < 6; i++) {
-      navLines[i].classList.remove("navLineAnimate")
+      navLines[i].classList.remove("navLineAnimate");
    }
 
    botNavText.classList.remove("move-bot-nav-txt");
@@ -335,7 +330,7 @@ const returnDefault = (iconName) => {
 const getBlog = name => document.getElementById(name);
 const getBlogContent = blog => blog.querySelectorAll(".modal-content");
 const getBlogContainer = () => document.querySelectorAll(".blog-post");
-const getBlogButton = () => document.querySelectorAll(".close-modal")
+const getBlogButton = () => document.querySelectorAll(".close-modal");
 
 const showBlog = (name, index) => {
    const blogContainer = getBlogContainer()[index];
@@ -350,10 +345,10 @@ const showBlog = (name, index) => {
 
       ignorePointer(blogContainer);
       blog.classList.remove("hide-blog");
-      blogButton.classList.add("show-blog-btn")
+      blogButton.classList.add("show-blog-btn");
       blog.classList.add("show-blog");
       for (let i = 0; i < blogContent.length; i++) {
-         blogContent[i].classList.add("show-modal-content")
+         blogContent[i].classList.add("show-modal-content");
       };
    }
 }
@@ -361,18 +356,18 @@ const showBlog = (name, index) => {
 const closeBlog = (name, index) => {
    const blogContainer = getBlogContainer()[index];
    const blog = getBlog(name);
-   const blogContent = getBlogContent(blog)
+   const blogContent = getBlogContent(blog);
 
    blog.classList.remove("show-blog");
    void blog.offsetWidth;
    blog.classList.add("hide-blog");
    for (let i = 0; i < blogContent.length; i++) {
-      blogContent[i].classList.remove("show-modal-content")
+      blogContent[i].classList.remove("show-modal-content");
    };
    setTimeout(() => {
       resumePointer(blogContainer);
       blog.classList.remove("hide-blog");
-      document.getElementsByTagName("body")[0].classList.remove("no-scroll")
+      document.getElementsByTagName("body")[0].classList.remove("no-scroll");
    }, 2000);
 }
 
@@ -381,6 +376,8 @@ const closeBlog = (name, index) => {
 const cvIcon = document.getElementsByClassName("cv-cont")[0];
 const recaptchaBox = document.getElementsByClassName("recaptcha-box")[0];
 const contactForm = document.getElementById("message-box");
+const submitMessageBox = document.getElementsByClassName("submit-form-msg")[0];
+const submitText = document.getElementsByClassName("submit-text")[0];
 
 const hoverCvIcon = () => {
    cvIcon.classList.add("hover-cv-icon");
@@ -391,5 +388,31 @@ const unhoverCvIcon = () => {
 }
 
 const showCaptcha = () => {
-   recaptchaBox.classList.add("show-captcha")
+   recaptchaBox.classList.add("show-captcha");
+}
+
+const showSubmitMessage = (status) => {
+   submitMessageBox.classList.add("submit-form-clicked");
+   submitText.innerHTML = status;
+}
+
+const closeSubmitMessage = () => {
+   submitMessageBox.classList.remove("submit-form-clicked");
+}
+
+const contactFormHandler = () => {
+   document.getElementsByClassName('contact-form')[0].addEventListener('submit', function (event) {
+      event.preventDefault();
+      // generate the contact number value
+      console.log(grecaptcha.getResponse().length)
+      if (grecaptcha.getResponse().length > 1)
+      {
+         showSubmitMessage("Thank you for submitting your enquiry.");
+         this.contact_number.value = Math.random() * 100000 | 0;
+         emailjs.sendForm('gmail', 'portfolio', this);
+      }
+      else {
+         showSubmitMessage("There was an error with your captcha. Please try again.");
+      }
+   });
 }
